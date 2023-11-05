d5c059247e5d08e26bf153af08646028a8f90bf0
Oszlik Tamás <70895240+Hadarnoon@users.noreply.github.com>
2023-11-05 10:33:20 +0100

    Add files via upload

---
+++ Szoftver/Szoftver/Program.cs
@@ -0,0 +1,85 @@
+﻿using Szoftver;
+
+static List<Szoftver1> KozelLegjobbak(List<Szoftver1> a)
+{
+	var f8 = a.Where(b => b.Ertekeles == a.Max(m => m.Ertekeles) - 0.1);
+	var kozelLegjobbak = new List<Szoftver1>();
+	foreach (var e in f8)
+	{
+		kozelLegjobbak.Add(e);
+	}
+	return kozelLegjobbak;
+}
+
+static double AtlagAdobeBrutto(List<Szoftver1> a)
+{
+	var f10 = a.Where(b => b.Nev.Contains("Adobe")).Average(c => c.BruttoAr);
+	return f10;
+}
+
+static List<Szoftver1> DragaRosszak(List<Szoftver1> a)
+{
+	var f12 = a.Where(b => b.NettoAr > 500 && b.Ertekeles < 9);
+	var dragaRosszak = new List<Szoftver1>();
+	foreach (var item in f12)
+	{
+		dragaRosszak.Add(item);
+	}
+	return dragaRosszak;
+}
+
+static void kiiras(List<Szoftver1> a)
+{
+	var f13 = a.Where(b => b.Fizetos == "fizetős").Take(10);
+	using var sw = new StreamWriter(@"..\..\..\src\kiiras.txt");
+	foreach (var item in f13)
+	{
+		sw.WriteLine(item);
+	}
+}
+
+var szoftverek = new List<Szoftver1>();
+using var sr = new StreamReader(@"..\..\..\src\szoftverek.txt");
+_ = sr.ReadLine();
+while (!sr.EndOfStream) szoftverek.Add(new Szoftver1(sr.ReadLine()));
+
+foreach (var s in szoftverek) Console.WriteLine(s);
+
+Console.WriteLine("\n7.feladat: ");
+var joAntivirus = new List<Szoftver1>();
+foreach (var s in szoftverek)
+{
+	if (s.Ertekeles > 8.5 && s.Kategoria == "antivírus")
+	{
+		joAntivirus.Add(s);
+	}
+}
+Console.WriteLine($"Ennyi 8,5-nél nagyobbra értékelt antivírus van: {joAntivirus.Count}");
+
+Console.WriteLine("\n8.feladat: ");
+
+var kozelLegjobbak = KozelLegjobbak(szoftverek);
+foreach (var item in kozelLegjobbak)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("\n10.feladat:");
+var f10 = AtlagAdobeBrutto(szoftverek);
+Console.WriteLine($"Adobe termékek átlag bruttó ára: {f10}");
+
+Console.WriteLine("\n12.feladat: ");
+var dragaRosszak = DragaRosszak(szoftverek);
+if (dragaRosszak != null)
+{
+	foreach (var item in dragaRosszak)
+	{
+		Console.WriteLine($"{item.Azonosito}, ");
+	}
+}
+else
+{
+    Console.WriteLine("Nincs ilyen");
+}
+
+kiiras(szoftverek);
---
+++ Szoftver/Szoftver/Szoftver.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szoftver
+{
+    internal class Szoftver1
+    {
+        public int Azonosito { get; set; }
+        public string Nev { get; set; }
+        public string Fizetos { get; set; }
+        public string KompatibilisOP { get; set; }
+        public string Kategoria { get; set; }
+        public double Ertekeles { get; set; }
+        public double NettoAr { get; set; }
+        public double Adokulcs { get; set; }
+        public double BruttoAr { get; set; }
+
+        public Szoftver1(string sor)
+        {
+            var v = sor.Split('|');
+            this.Azonosito = int.Parse(v[0]);
+            this.Nev = v[1];
+            this.Fizetos = v[2];
+            this.KompatibilisOP = v[3];
+            //var kompAtmeneti = v[3].Split(",");
+            //foreach (var item in kompAtmeneti)
+            //{
+            //    this.KompatibilisOP.Add(item);
+            //}
+            this.Kategoria = v[4];
+            this.Ertekeles = double.Parse(v[5]);
+            this.NettoAr = double.Parse(v[6]);
+            this.Adokulcs = double.Parse(v[7]);
+            this.BruttoAr = this.NettoAr * (this.Adokulcs / 100);
+        }
+
+        public override string ToString()
+        {
+            return $"{Azonosito} | {Nev} | {Fizetos} | {KompatibilisOP} | {Kategoria} | {Ertekeles - 0.1} | {NettoAr} | {Adokulcs}";
+        }
+    }
+}
